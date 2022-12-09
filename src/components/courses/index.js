56c80c97009aa4d66572f369fa82239c5f2620d5
nicolem321 <nicolem321323@gmail.com>
2022-12-09 14:35:33 -0500

    added picture

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -5,12 +5,17 @@ const Courses = () => {
     return (
         <div class="box">
             <h1 className="nameWrapper1">Courses Taught</h1>
-            <div class="box1">
+            <div className="box1">
+            <div className = "box3">
                 • <b>Head Teaching Assistant</b>: <em>Data Structures and Fundementals of Computing</em>, Prof. Iraklis Tsekourakis, Fall2022<br></br>
                 • <b>Teaching Assistant</b>: <em>Data Structures and Fundementals of Computing</em>, Prof. Antonella Di Lillo, Spr2022<br></br>
                 • <b>Head Teaching Assistant</b>: <em>Problem Solving in Python</em>, Prof. Iraklis Tsekourakis, Fall2021 - Summer2022<br></br>
                 • <b>Teaching Assistant</b>: <em>Econometrics</em>, Prof. YinChu Zhu, Spr2022<br></br>
                 • <b>Teaching Assistant</b>: <em>Problem Solving in Python</em>, Prof. Timothy Hickey, Spr2021<br></br>
+            </div>
+            <div className = "box2">
+                <img src={require('./teaching.png')} width="40%" height="30%" />
+            </div>
             </div>
         </div>
     );
